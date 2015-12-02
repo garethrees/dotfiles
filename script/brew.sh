586dd66551ff2f64e00f7b8c826c1fafb6de4589
Gareth Rees <gareth.h.rees@gmail.com>
2015-12-02 22:09:39 +0000

    Update brew.sh

--- script/brew.sh
+++ script/brew.sh
@@ -14,24 +14,36 @@ brew upgrade
 brew tap thoughtbot/formulae
 brew tap phinze/cask
 
-brew install brew-cask
+brew install ack
+brew install automake
+brew install caskroom/cask/brew-cask
 brew install chruby
+brew install cloc
 brew install coreutils
-brew install datamash
-# brew install elixir
-# brew install erlang
+brew install homebrew/science/datamash
 brew install git
-brew install gitsh
+brew install thoughtbot/formulae/gitsh
+brew install gtk+
+brew install heroku-toolbelt
 brew install imagemagick
-# brew install mysql
-brew install openssl
-brew install rcm
-# brew install rbenv
-brew install readline
+brew install iperf
+brew install jq
+brew install libidn
+brew install libyaml
+brew install lnav
+brew install mobile-shell
+brew install mysql
+brew install nmap
+brew install node
+brew install ossp-uuid
+brew install postgresql
+brew install python
 brew install reattach-to-user-namespace
-# brew install ruby-build
 brew install ruby-install
-# brew install sqlite
+brew install spark
+brew install sqlite
+brew install ssh-copy-id
+brew install st
 brew install tmux
 brew install tree
 brew install vim
@@ -39,17 +51,24 @@ brew install wget --enable-iri
 
 brew cask install alfred
 brew cask alfred link
+brew cask install carbon-copy-cloner
+brew cask install cyberghost
 brew cask install dropbox
-brew cask install firefox
 brew cask install f-lux
+brew cask install firefox
 brew cask install gitx-rowanj
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install jdiskreport
+brew cask install karabiner
+brew cask install licecap
 brew cask install monolingual
 brew cask install nvalt
 brew cask install paparazzi
+brew cask install postico
 brew cask install sequel-pro
+brew cask install selfcontrol
+brew cask install sketchup
 brew cask install skype
 brew cask install spectacle
 brew cask install spotify
@@ -58,5 +77,6 @@ brew cask install the-unarchiver
 brew cask install vagrant
 brew cask install virtualbox
 brew cask install vlc
+brew cask install xquartz
 
 brew cleanup
