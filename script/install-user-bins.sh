c5b6a7e9452b3a308d2aad364c2f53716663c1c1
Gareth Rees <gareth@garethrees.co.uk>
2016-01-03 11:51:42 +0000

    Add note about copying debian diff-highlight

--- script/install-user-bins.sh
+++ script/install-user-bins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install diff-highlight on Ubuntu/Debian
+# Copy it so that we can add +x perms
 if [[ -f /usr/share/doc/git/contrib/diff-highlight/diff-highlight ]]
   then
     cp -uv /usr/share/doc/git/contrib/diff-highlight/diff-highlight "$HOME/bin"
