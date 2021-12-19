0d75493fd4a25292bb2f620018476e84010a6580
Gareth Rees <gareth@garethrees.co.uk>
2021-12-19 17:11:18 +0000

    Fix diff-highlight installer
    Oops, `/opt/homebrew` is a directory.

--- script/install-user-bins.sh
+++ script/install-user-bins.sh
@@ -10,7 +10,7 @@ if [[ -f /usr/share/doc/git/contrib/diff-highlight/diff-highlight ]]
 fi
 
 # Install diff-highlight on OS X
-if [[ -f /opt/homebrew ]]; then
+if [[ -d /opt/homebrew ]]; then
   bin_path="$(find /opt/homebrew -name diff-highlight -type f | head -n 1)"
   ln -sfnv "$bin_path" "$HOME/bin/diff-highlight"
 fi
