b8064a71928754287edd66409316614debdd8a46
Gareth Rees <gareth@garethrees.co.uk>
2021-12-19 17:15:45 +0000

    Only unload LaunchAgents if already linked
    First time install shows errors because the target doesn’t exist to
    unload.

--- script/install-launch-agents.sh
+++ script/install-launch-agents.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-if [[ -d "$HOME/Library/LaunchAgents" ]]
-  then
-    for source in "$DOTFILES_DIR/lib/LaunchAgents/"*; do
-      target="$HOME/Library/LaunchAgents/$(basename "$source")"
-      launchctl unload "$target"
-      ln -sFv "$source" "$target"
-      launchctl load "$target"
-    done
-fi
+if [[ -d "$HOME/Library/LaunchAgents" ]]; then
+  for source in "$DOTFILES_DIR/lib/LaunchAgents/"*; do
+    target="$HOME/Library/LaunchAgents/$(basename "$source")"
 
+    # Only try to unload if already installed
+    if [[ -L "$target" ]]; then
+      launchctl unload "$target"
+    fi
+
+    ln -sFv "$source" "$target"
+    launchctl load "$target"
+  done
+fi
