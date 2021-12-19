2da5a349f399963783768991bff5604979257b49
Gareth Rees <gareth@garethrees.co.uk>
2021-12-19 17:33:37 +0000

    Fix installing Folder Actions
    Directory doesn’t exist on first install, so just make sure it’s
    created.
    
    Also use GNU ln

--- script/install-folder-actions.sh
+++ script/install-folder-actions.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Not created by default on fresh install
+if [[ -d "$HOME/Library" ]]; then
+  mkdir -p "$HOME/Library/Workflows/Applications/Folder Actions"
+fi
+
 if [[ -d "$HOME/Library/Workflows/Applications/Folder Actions" ]]
   then
     for source in "$DOTFILES_DIR/lib/Folder Actions/"*; do
       target="$HOME/Library/Workflows/Applications/Folder Actions/$(basename "$source")"
-      ln -sFhv "$source" "$target"
+      ln -sFnv "$source" "$target"
     done
 fi
