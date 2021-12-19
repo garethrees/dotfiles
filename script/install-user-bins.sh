c1144fbed0eefa7f13ac7646cdda844e76854035
Gareth Rees <gareth@garethrees.co.uk>
2021-12-19 17:08:08 +0000

    Fix diff-highlight installer
    Moved location and now no static location so need to find it.
    
    Could add some error checking but let’s see if we need it first.

--- script/install-user-bins.sh
+++ script/install-user-bins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "==> Installing diff-highlight..."
 # Install diff-highlight on Ubuntu/Debian
 # Copy it so that we can add +x perms
 if [[ -f /usr/share/doc/git/contrib/diff-highlight/diff-highlight ]]
@@ -9,7 +10,7 @@ if [[ -f /usr/share/doc/git/contrib/diff-highlight/diff-highlight ]]
 fi
 
 # Install diff-highlight on OS X
-if [[ -f /usr/local/var/homebrew/linked/git/share/git-core/contrib/diff-highlight/diff-highlight ]]
-  then
-    ln -sfv /usr/local/var/homebrew/linked/git/share/git-core/contrib/diff-highlight/diff-highlight "$HOME/bin/diff-highlight"
+if [[ -f /opt/homebrew ]]; then
+  bin_path="$(find /opt/homebrew -name diff-highlight -type f | head -n 1)"
+  ln -sfnv "$bin_path" "$HOME/bin/diff-highlight"
 fi
